d0de869d30ed19723d889d1b92e62b6b3577aab5
codebudy5247 <a.shekhar5247@gmail.com>
2024-06-26 17:14:47 +0530

    max subarray sum

--- Javascript/Code-Challenges/arrays/Get-second-largest-num.js
+++ Javascript/Code-Challenges/arrays/Get-second-largest-num.js
@@ -8,16 +8,34 @@
  * @param {number[]} arr - The input array.
  * @returns {number} The second largest number in the array, or -1 if there is no second largest number.
  */
-function secondLargestNumber(arr){
-  const uniqueElement = Array.from(new Set(arr))
-  
-  if(arr.length > 2){
-    uniqueElement.sort((a,b) => b-a)
+
+// Method 1
+function secondLargestNumber(arr) {
+  const uniqueElement = Array.from(new Set(arr));
+
+  if (arr.length > 2) {
+    uniqueElement.sort((a, b) => b - a);
   } else {
-    return -1
+    return -1;
   }
-  return uniqueElement[1]
+  return uniqueElement[1];
 }
 
-console.log(secondLargestNumber([2,6,5,9,3,9]))
-console.log(secondLargestNumber([10,5,10]))
+// Optimized method
+function secondLargestNumberOptimized(arr) {
+  let largest = Number.NEGATIVE_INFINITY;
+  let secondLargest = Number.NEGATIVE_INFINITY;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] != largest && arr[i] > secondLargest) {
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest;
+}
+
+console.log(secondLargestNumber([12, 35, 1, 10, 34, 1]));
+console.log(secondLargestNumber([10, 5, 10]));
