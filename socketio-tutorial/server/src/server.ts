b46c956d6e7c91125d76a45f4d6ed3d9ca5209aa
codebudy5247 <a.shekhar5247@gmail.com>
2024-08-30 11:51:51 +0530

    :hammer:

--- socketio-tutorial/server/src/server.ts
+++ socketio-tutorial/server/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import SocketService from "./socket";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('<h1>Server is running!!!</h1>');
@@ -10,25 +10,6 @@ app.get('/', (req, res) => {
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  const socketService = new SocketService(server);
+  socketService.initListeners();
 });
-
-const socketService = new SocketService();
-socketService.io.attach(server);
-socketService.initListeners()
-
-
-// async function init() {
-//   const socketService = new SocketService();
-//   const httpServer = http.createServer();
-//   const PORT = 3000;
-
-//   socketService.io.attach(httpServer);
-
-//   httpServer.listen(PORT, () => {
-//     console.log(`HTTP Server is running on http://localhost:${PORT}`);
-//   });
-
-//   socketService.initListeners()
-// }
-
-// init();
--- socketio-tutorial/server/src/socket.ts
+++ socketio-tutorial/server/src/socket.ts
@@ -1,45 +1,42 @@
 import { Server } from "socket.io";
-import Redis from "ioredis";
+import Redis, { Redis as RedisClient } from "ioredis";
+import { Server as HttpServer } from "http";
 
 const REDIS_URL =
+  process.env.REDIS_URL ||
   "redis://default:5698d6027eb44f6e98b86259b06e29d3@apn1-useful-katydid-33905.upstash.io:33905";
-/* Username: default
-   Password: 5698d6027eb44f6e98b86259b06e29d3
-   Host: apn1-useful-katydid-33905.upstash.io
-   Port: 33905 
-*/
-
-const pub = new Redis(REDIS_URL);
-const sub = new Redis(REDIS_URL);
 
 class SocketService {
   private _io: Server;
+  private _pub: RedisClient;
+  private _sub: RedisClient;
 
-  constructor() {
-    this._io = new Server({
+  constructor(server: HttpServer) {
+    this._io = new Server(server, {
       cors: {
         allowedHeaders: ["*"],
         origin: "*",
       },
     });
-    sub.subscribe("MESSAGES");
+
+    this._pub = new Redis(REDIS_URL);
+    this._sub = new Redis(REDIS_URL);
+    this._sub.subscribe("MESSAGES");
   }
 
   public initListeners() {
-    const io = this.io;
-    io.on("connect", (socket) => {
+    this._io.on("connect", (socket) => {
       console.log("New socket connected", socket.id);
 
       socket.on("event:message", async ({ message }: { message: string }) => {
-        console.log("New message recieved:", message);
-
-        await pub.publish("MESSAGES", JSON.stringify({ message }));
+        console.log("New message received:", message);
+        await this._pub.publish("MESSAGES", JSON.stringify({ message }));
       });
     });
 
-    sub.on("message", (channel, message) => {
+    this._sub.on("message", (channel, message) => {
       if (channel === "MESSAGES") {
-        io.emit("message", message);
+        this._io.emit("message", message);
       }
     });
   }
