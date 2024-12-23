ab061aed07c5b5d24b5da67456611c13d109a498
codebudy5247 <a.shekhar5247@gmail.com>
2024-12-23 13:41:06 +0530

    JWT auth:unit test for user registration

--- nodejs/jwt-authentication/src/__tests__/unit/auth.test.ts
+++ nodejs/jwt-authentication/src/__tests__/unit/auth.test.ts
@@ -2,49 +2,91 @@ import supertest from "supertest";
 import { createServer } from "../../server";
 import { db } from "../../database";
 
-describe("Auth routes", () => {
-  describe("POST api/v1/auth/register", () => {
-    const server = createServer();
-    const request = supertest(server);
-    let newUser: any;
-    beforeAll(async () => {
-      supertest(createServer());
-      newUser = {
-        name: "aditya",
-        email: "aditya@example.com",
-        password: "password1",
-      };
-    });
+describe("Auth Routes", () => {
+  const server = createServer();
+  const request = supertest(server);
+  const avatarUrl =
+    "https://lh3.googleusercontent.com/ogw/AF2bZyjFJIr2Mw4DiejAL2zjb1ZLa7FVVmy1lshG8ErCO3GPLFk=s32-c-mo";
+  const defaultTestUser = {
+    name: "aditya",
+    email: "aditya@example.com",
+    password: "password123",
+  };
 
-    test("should return 201 and successfully register user if request data is ok", async () => {
-      const res = await request
-        .post("/api/v1/user/register")
-        .send(newUser)
-        .expect(200);
-      expect(res.body).toEqual({
+  let createdUserId: string | null = null;
+
+  // beforeAll(() => {
+
+  // });
+
+  afterAll(async () => {
+    if (createdUserId) {
+      await db.user.delete({
+        where: { id: createdUserId },
+      });
+    }
+  });
+
+  const testUserRegistration = async (
+    userData: Record<string, string>,
+    expectedStatusCode: number
+  ) => {
+    return request
+      .post("/api/v1/user/register")
+      .send(userData)
+      .expect(expectedStatusCode);
+  };
+
+  describe("User Registration POST /v1/auth/register", () => {
+    it("should return 200 and successfully register a user with valid data", async () => {
+      const response = await testUserRegistration(defaultTestUser, 200);
+
+      expect(response.body).toEqual({
         id: expect.anything(),
-        name: newUser.name,
-        email: newUser.email,
-        avatar:
-          "https://lh3.googleusercontent.com/ogw/AF2bZyjFJIr2Mw4DiejAL2zjb1ZLa7FVVmy1lshG8ErCO3GPLFk=s32-c-mo",
+        name: defaultTestUser.name,
+        email: defaultTestUser.email,
+        avatar: avatarUrl,
         role: "USER",
         createdAt: expect.anything(),
         updatedAt: expect.anything(),
       });
 
+      createdUserId = response.body.id;
+
       const dbUser = await db.user.findFirst({
-        where: {
-          id: res.body.id,
-        },
+        where: { id: response.body.id },
       });
+
       expect(dbUser).toBeDefined();
       expect(dbUser).toMatchObject({
-        name: newUser.name,
-        email: newUser.email,
+        name: defaultTestUser.name,
+        email: defaultTestUser.email,
         role: "USER",
-        avatar:
-          "https://lh3.googleusercontent.com/ogw/AF2bZyjFJIr2Mw4DiejAL2zjb1ZLa7FVVmy1lshG8ErCO3GPLFk=s32-c-mo",
+        avatar: avatarUrl,
       });
+    });
+
+    it("should return 422 if email is invalid", async () => {
+      const invalidUser = { ...defaultTestUser, email: "invalidEmail" };
+      await testUserRegistration(invalidUser, 422);
+    });
+
+    it("should return 400 if email is already used", async () => {
+      const duplicateEmailUser = { ...defaultTestUser };
+      await testUserRegistration(duplicateEmailUser, 400);
+    });
+
+    it("should return 422 if password length is less than 6 characters", async () => {
+      const shortPasswordUser = { ...defaultTestUser, password: "1234" };
+      await testUserRegistration(shortPasswordUser, 422);
+    });
+
+    it("should return 422 if password does not contain both letters and numbers", async () => {
+      const noLettersPassword = { ...defaultTestUser, password: "123456" };
+      const noNumbersPassword = { ...defaultTestUser, password: "password" };
+
+      await testUserRegistration(noLettersPassword, 422);
+      await testUserRegistration(noNumbersPassword, 422);
     });
   });
 });
--- nodejs/jwt-authentication/src/modules/user/user.schema.ts
+++ nodejs/jwt-authentication/src/modules/user/user.schema.ts
@@ -1,7 +1,13 @@
 import { z } from "zod";
 
 export const RegisterSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string().min(6),
+  name: z.string({ required_error: "Name is required" }).min(5).max(20),
+  email: z.string({ required_error: "Email is required" }).email(),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(6, "Password must be at least 6 characters long")
+    .regex(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/,
+      "Password must contain both letters and numbers"
+    ),
 });
