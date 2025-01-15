e581d1e5df03fc644d48dbc36a26db9c65369ca0
codebudy5247 <a.shekhar5247@gmail.com>
2025-01-15 11:45:58 +0530

    redux-toolkit example

--- reactjs/redux-toolkit-example/src/redux/hook.ts
+++ reactjs/redux-toolkit-example/src/redux/hook.ts
@@ -1,5 +1,10 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "./store";
 
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+// The useAppDispatch and useAppSelector hooks are custom hooks that provide type information for the 
+// useDispatch and useSelector hooks from react-redux.
+// The useAppDispatch hook returns the dispatch function with the correct type information.
+// The useAppSelector hook returns the useSelector hook with the correct RootState type.
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
